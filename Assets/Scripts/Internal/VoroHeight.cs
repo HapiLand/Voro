ef68bc36c9ecb41a00f03c742951f1050ceaec7a
hapi <220979330+HapiLand@users.noreply.github.com>
2025-08-23 14:17:54 +0100

    VoroHeight updated to use array of IConfig

--- Assets/Scripts/Internal/VoroHeight.cs
+++ Assets/Scripts/Internal/VoroHeight.cs
@@ -17,23 +17,25 @@ public class VoroHeight {
     
     // ToDo store the instructions as a collection
     
-    NoiseCfg _noiseCfg;
+    // the new way to store configruations for each instruction
+    // this originated from the json file
+    // now when VoroHeight can find the height, it will iterate through this array
+    public IConfig[] Configs;
+    
     Noise _noiseNde;
-    
-    SlopeCfg _slopeCfg;
     Slope _slopeNde;
-    
-    TerraceCfg _terraceCfg;
     Terrace _terraceNde;
     
     public VoroHeight(JsonConfig config) {
         // generate the various IConfig structs found in the config data
         // these configurations contain the data in the json file
         // these are used to configure each instruction to control its output
-        _slopeCfg = config.Configs[0] is SlopeCfg ? (SlopeCfg)config.Configs[0] : default;
+        /*_slopeCfg = config.Configs[0] is SlopeCfg ? (SlopeCfg)config.Configs[0] : default;
         _noiseCfg = config.Configs[1] is NoiseCfg ? (NoiseCfg)config.Configs[1] : default;
-        _terraceCfg = config.Configs[2] is TerraceCfg ? (TerraceCfg)config.Configs[2] : default;
+        _terraceCfg = config.Configs[2] is TerraceCfg ? (TerraceCfg)config.Configs[2] : default;*/
 
+        Configs = config.Configs;
+        
         _slopeNde = new Slope();
         _noiseNde = new Noise();
         _terraceNde = new Terrace();
@@ -47,7 +49,6 @@ public class VoroHeight {
          * this new approach will aim to clear out all the Slope/Noise/Terrace code
          * which is hardcoded into this class
          */
-        
         
         // each height value for each point
         heightArr = new float[pointArr.points.Length];
@@ -69,33 +70,32 @@ public class VoroHeight {
         void FindHeightAtPosition(Vector2 pos, out float height) {
             // initial height value at 0
             height = 0;
-            
-            // there are 3 instructions in this config - slope, noise, terrace
-            var instructionAmount = 3;
 
-            // iterate through the expression to execute each instruction
-            for (var i = 0; i < instructionAmount; i++) {
-                // the current iteration has the instruction to create a slope
-                if (i == 0) {
-                    _slopeNde.ComputeHeight(_slopeCfg, new Vector3(pos.x, height, pos.y), out var slopeHeight);
+            for (var i = 0; i < Configs.Length; i++) {
+                // read the current configuration
+                IConfig cfg = Configs[i];
+                
+                // determine the actual IConfig struct this cfg is
+                if (cfg is SlopeCfg slope) {
+                    // the IConfigs type is found
+                    // ths configuration is provided to the Instruction
+                    // the result of this is that height is computed where the resulting height
+                    // has been controlled by the instruction based on what the config says
+                    // for a Slope effect, the config says how steep the slope is and what its direction is
+                    
+                    _slopeNde.ComputeHeight(slope, new Vector3(pos.x, height, pos.y), out var slopeHeight);
                     height += slopeHeight;
                 }
-                
-                // the current iteration has the instruction to apply noise
-                if (i == 1) {
-                    _noiseNde.ComputeHeight(_noiseCfg, new Vector3(pos.x, height, pos.y), out var noiseHeight);
+                else if (cfg is NoiseCfg noise) {
+                    _noiseNde.ComputeHeight(noise, new Vector3(pos.x, height, pos.y), out var noiseHeight);
                     height += noiseHeight;
                 }
-                
-                // the current iteration has the instruction to apply a terrace effect
-                if (i == 2) {
-                    _terraceNde.ComputeHeight(_terraceCfg, new Vector3(pos.x, height, pos.y), out var terraceHeight);
+                else if (cfg is TerraceCfg terrace) {
+                    _terraceNde.ComputeHeight(terrace, new Vector3(pos.x, height, pos.y), out var terraceHeight);
                     if (terraceHeight != 0.0) {
                         height += terraceHeight;
                         height /= 2;
                     }
-                    
-
                 }
             }
         }
