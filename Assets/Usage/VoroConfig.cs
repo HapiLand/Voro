fe6d80bd5c309fefca07e7e8f758a3142fe792ce
Hapi <220979330+HapiLand@users.noreply.github.com>
2025-11-21 13:25:19 +0000

    removed unnecesary files

--- Assets/Usage/VoroConfig.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-namespace Usage {
-[CreateAssetMenu(fileName = "Configuration", menuName = "Voro/Config/New", order = 0)]
-public class VoroConfig : ScriptableObject {
-    public string configName;
-}
-}
